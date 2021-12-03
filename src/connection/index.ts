1a9aa0cbcc008f19576fd05cd939c0bee27bca7f
Phong Vuong <pyriter.io@gmail.com>
2021-12-02 23:16:02 -0800

    renamed main to index

---
+++ src/connection/index.ts
@@ -0,0 +1,4 @@
+export * as client from "./client"
+export * as connect from "./connect"
+export * as credentialsInterceptor from "./credentialsInterceptor"
+export * as routes from "./routes.config"
--- src/main.ts
+++
@@ -1,2 +0,0 @@
-export * as authenticate from './api/authenticate';
-export * as accounts from './api/accounts';
