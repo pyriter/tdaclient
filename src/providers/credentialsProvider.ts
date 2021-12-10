00be882b96ee362bc9634fd6f0ccfeefa9b9d7df
Phong Vuong <pyriter.io@gmail.com>
2021-12-10 00:16:25 -0800

    Update TdaCredential type

--- src/providers/credentialsProvider.ts
+++ src/providers/credentialsProvider.ts
@@ -1,13 +1,13 @@
 export interface TdaCredential {
-  access_token: string;
-  refresh_token: string;
-  scope: string;
-  expires_in: number;
-  refresh_token_expires_in: number;
-  token_type: string;
-  modified_date: number;
-  client_id: string;
-  redirect_uri: string;
+  access_token?: string;
+  refresh_token?: string;
+  scope?: string;
+  expires_in?: number;
+  refresh_token_expires_in?: number;
+  token_type?: string;
+  modified_date?: number;
+  client_id?: string;
+  redirect_uri?: string;
 }
 
 export interface CredentialProvider {
